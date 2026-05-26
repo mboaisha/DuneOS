d27565395ac37889aabb79d56122619360ecc400
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-05-26 01:11:46 -0500

    Deleting cruft and trying something

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,16 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install packages from Fedora repositories
+dnf5 install -y tmux neovim
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 -y copr enable mboaisha/assortment
+dnf5 -y install lavat
+dnf5 -y copr disable mboaisha/assortment
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# Add Terra repo
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+# Yeet sway and replace it with Swayfx
+sudo dnf swap sway swayfx --allowerasing --setopt=protected_packages=
 
 systemctl enable podman.socket
