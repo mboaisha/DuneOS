bb1ed4db4ea4d7a55f07845376c7e9effbc2192a
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-05-26 14:15:17 -0500

    Might as well

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Install packages from Fedora repositories
-dnf5 install -y tmux neovim curl wget wlogout fuzzel 
+dnf5 install -y tmux neovim curl wget wlogout fuzzel fastfetch
 
 # Add my personal COPR and install lavat
 dnf5 -y copr enable mboaisha/assortment
