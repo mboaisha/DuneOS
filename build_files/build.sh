6cb396a05a7258e44722c04978ddf0a6ee6a0b04
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-05-26 01:50:37 -0500

    Actually build and install Wayle

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,17 @@ dnf5 swap -y sway swayfx --allowerasing --setopt=protected_packages=
 # Install Rust to build Wayle
 dnf5 install -y rust cargo
 
+# Deps for building Wayle
+sudo dnf install git cmake pkgconf-pkg-config gtk4-devel gtk4-layer-shell-devel \
+  gtksourceview5-devel pulseaudio-libs-devel fftw-devel pipewire-devel \
+  systemd-devel clang gcc libxkbcommon-devel
+
+# Build Wayle
+git clone https://github.com/wayle-rs/wayle
+cd wayle
+cargo install --path wayle
+cargo install --path crates/wayle-settings
+
 systemctl enable podman.socket
 
 dnf5 clean all
