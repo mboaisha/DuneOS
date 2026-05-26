c0596581799fa4fb465ea977e8fce503be07b310
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-05-26 01:28:08 -0500

    Attempting to install Rust

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Install packages from Fedora repositories
-dnf5 install -y tmux neovim
+dnf5 install -y tmux neovim curl
 
 dnf5 -y copr enable mboaisha/assortment
 dnf5 -y install lavat
@@ -16,6 +16,7 @@ dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/te
 # Note: You might have stuttering issues if you use this in a virtual machine
 dnf5 swap -y sway swayfx --allowerasing --setopt=protected_packages=
 
-
+# Install Rust to build Wayle
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 systemctl enable podman.socket
