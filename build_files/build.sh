03d313e728ad971a077a81839c84e92223a5ffb4
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-06-13 22:48:35 -0500

    Removing cruft

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,6 @@ dnf5 -y install tmux                     \
                 neovim                   \
                 curl                     \
                 wget                     \
-                wlogout                  \
                 fuzzel                   \
                 fastfetch                \
                 chezmoi                  \
@@ -19,7 +18,6 @@ dnf5 -y install tmux                     \
                 xorg-x11-server-Xwayland \
                 bat                      \
                 nemo                     \
-                alacritty                \
                 kitty                    \
                 wlsunset                 \
                 cargo                    \
@@ -42,10 +40,6 @@ dnf5 -y copr disable mboaisha/assortment
 
 # Add Terra repo
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-
-# Noctalia shell
-# Requires Terra repo
-#dnf5 install -y noctalia-shell
 
 # ghostty terminal emulator
 # Requires Terra repo
@@ -74,51 +68,14 @@ dnf5 swap -y sway swayfx --allowerasing --setopt=protected_packages=
 # Requires Terra repo
 dnf5 -y install bitwarden-cli
 
-# Change to the default upstream config
-##dnf5 swap -y sway-config sway-config-upstream --allowerasing
-# Get rid of deadweight
-##dnf5 remove -y wmenu sway-wallpapers
-
-# Install awww, wallpaper daemon
-# Requires Terra repo
-#dnf5 install -y awww
-
-# Install waypaper, GUI for wallpaper setting and such
-# Requires Terra repo
-#dnf5 install -y waypaper
-
 # Install VSCodium
 # Note: .repo is already defined in the image
 dnf5 install -y codium
 
 dnf5 install -y tailscale
-#############################
-# This does not seem to be correct... They won't install
-#############################
+
 # Add flathub
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-# Install Bazaar
-#flatpak install --noninteractive flathub io.github.kolunmi.Bazaar
-#flatpak preinstall -y
-
-#############################
-# There is probably a cleaner way of doing this...
-#############################
-# Install Rust to build Wayle
-#dnf5 install -y rust cargo
-
-# Deps for building Wayle
-#dnf5 -y install git cmake pkgconf-pkg-config gtk4-devel gtk4-layer-shell-devel \
-#  gtksourceview5-devel pulseaudio-libs-devel fftw-devel pipewire-devel \
-#  systemd-devel clang gcc libxkbcommon-devel
-
-# Build and install Wayle
-#export CARGO_HOME=/var/tmp/cargo
-#git clone https://github.com/wayle-rs/wayle
-#cd wayle
-#cargo install --root /usr --path wayle
-#cargo install --root /usr --path crates/wayle-settings
-###############################
 
 systemctl enable podman.socket
 systemctl enable libvirtd.service
@@ -128,7 +85,5 @@ systemctl enable sshd.service
 systemctl enable tailscaled.service
 systemctl disable sddm
 systemctl enable greetd
-
-#systemctl --global enable awww-daemon.service
 
 dnf5 clean all
