3a88e4fcef44be915b233b57dfc71b3ab4f1d2e0
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-06-16 10:52:12 -0500

    Put swayfx back in and try to dms via systemd

--- build_files/build.sh
+++ build_files/build.sh
@@ -62,7 +62,7 @@ dnf5 -y install ubuntumono-nerd-fonts      \
 # Yeet sway and replace it with Swayfx
 # Note: You might have stuttering issues if you use this in a virtual machine
 # Requires Terra repo
-#dnf5 swap -y sway swayfx --allowerasing --setopt=protected_packages=
+dnf5 swap -y sway swayfx --allowerasing --setopt=protected_packages=
 
 # Bitwarden CLI
 # Requires Terra repo
@@ -85,5 +85,6 @@ systemctl enable sshd.service
 systemctl enable tailscaled.service
 systemctl disable sddm
 systemctl enable greetd
+systemctl enable dms
 
 dnf5 clean all
